1489b6d961b5be73f74ebdb97cbbfd496786b736
Damien Barchowsky <dbarchowsky@gmail.com>
2018-01-17 15:27:37 -0800

    Display statistics on Episode Details page. closes #17

--- bg_tracking/routes/episode_routes.py
+++ bg_tracking/routes/episode_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from peewee import *
 from bg_tracking.models import *
 
 episode_routes = Blueprint('episode_routes', __name__, template_folder='templates')
@@ -25,9 +26,34 @@ def details_view(episode_id):
     :type episode_id: int
     :return: Response
     """
-    e = Episode.get(Episode.id == episode_id)
-    bgs = Background.select().where(Background.episode == episode_id).order_by(Background.scene)
-    return render_template('episode_details.html', episode=e, bgs=bgs)
+    try:
+        e = (Episode
+             .select(Episode,
+                     fn.COUNT(Background.id).alias('bg_count'),
+                     fn.SUM(Background.hours).alias('total_hours'),
+                     fn.AVG(Background.hours).alias('avg_hours'))
+             .join(Background, JOIN.LEFT_OUTER)
+             .where(Episode.id == episode_id)
+             .get()
+             )
+    except Episode.DoesNotExist:
+        return render_template('error.html', error_msg='The requested episode was not found.')
+    else:
+        try:
+            bgs = (Background
+                   .select()
+                   .where(Background.episode == episode_id)
+                   .order_by(Background.scene)
+                   )
+        except Background.DoesNotExist:
+            err = 'Error retrieving BGs for episode “{}”.'.format(e.title)
+            return render_template('error.html', error_msg=err)
+        stats = {
+                'count': '{} BG{}'.format(e.bg_count, '' if e.bg_count == 1 else 's'),
+                'total_hours': '{:.2f} total hour{}'.format(e.total_hours, '' if e.total_hours == 1 else 's'),
+                'avg_hours': '{:.2f} hour{} per BG'.format(e.avg_hours, '' if e.total_hours == 1 else 's'),
+                }
+    return render_template('episode_details.html', episode=e, bgs=bgs, stats=stats)
 
 
 @episode_routes.route('/episode/<int:episode_id>/edit/')
---
+++ tests/test_episodes.py
@@ -0,0 +1,50 @@
+import unittest
+from peewee import *
+import bg_tracking
+from bg_tracking.models import *
+
+
+class EpisodeTestCase(unittest.TestCase):
+    """
+    Tests Show-related page content.
+    """
+    def setUp(self):
+        base_model.db.connect()
+
+    def tearDown(self):
+        base_model.db.close()
+
+    def test_episode_details_query(self):
+
+        episode_id = 1
+        bg_count = 26
+        show_title = 'Close Enough'
+        season = 1
+
+        # get most recent show
+        e = (Episode
+             .select(Episode,
+                     fn.COUNT(Background.id).alias('bg_count'),
+                     fn.SUM(Background.hours).alias('total_hours'),
+                     fn.AVG(Background.hours).alias('avg_hours'))
+             .join(Background, JOIN.LEFT_OUTER)
+             .where(Episode.id == episode_id)
+             .get()
+             )
+
+        self.assertEqual(e.id, episode_id)
+        self.assertEqual(e.bg_count, bg_count)
+        self.assertGreater(e.total_hours, 0)
+        self.assertGreater(e.avg_hours, 0)
+        self.assertEqual(e.show.title, show_title)
+        self.assertEqual(e.show.season, season)
+
+    def test_invalid_episode_id(self):
+
+        episode_id = 999
+        with self.assertRaises(Episode.DoesNotExist):
+            Episode.get(Episode.id == episode_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
